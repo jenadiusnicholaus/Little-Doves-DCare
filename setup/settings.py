691db73817241676ef77cb98b358be9fa022b7c7
jenadiusnicholaus <ucode888@gmail.com>
2022-03-27 21:35:51 +0300

    prepare to deploy

--- setup/settings.py
+++ setup/settings.py
@@ -134,9 +134,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
